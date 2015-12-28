59861f83c4bbd441fb808821cbd6f26442283151
kael_wu <wuqihan25@gmail.com>
2015-12-28 20:07:44 +0800

    rpc update

--- src/JsonRpc/Transport/BasicClient.php
+++ src/JsonRpc/Transport/BasicClient.php
@@ -5,43 +5,56 @@ namespace JsonRpc\Transport;
 class BasicClient
 {
 
-  public $output = '';
-  public $error = '';
+    public $output = '';
+    public $error = '';
 
 
-  public function send($method, $url, $json, $headers = array())
-  {
-
-    $header = 'Content-Type: application/json';
-
-    if (!in_array($header, $headers))
+    public function send($method, $url, $json, $headers = array())
     {
-      $headers[] = $header;
+
+        $header = 'Content-Type: application/json';
+
+        if (!in_array($header, $headers)) {
+            $headers[] = $header;
+        }
+
+//        $opts = array(
+//            'http' => array(
+//                'method' => $method,
+//                'header' => implode("\r\n", $headers),
+//                'content' => $json,
+//            )
+//        );
+//
+//        $context = stream_context_create($opts);
+//        #$response = @file_get_contents($url, false, $context);
+//        $response = file_get_contents($url, false, $context);
+
+        $ch = curl_init();
+        curl_setopt ($ch, CURLOPT_HTTPHEADER, implode("\r\n", $headers));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_URL, $url);
+            // post数据
+            curl_setopt($ch, CURLOPT_POST, 1);
+            // post的变量
+            curl_setopt($ch, CURLOPT_POSTFIELDS, ['content' => $json]);
+        } else {
+            curl_setopt($ch, CURLOPT_URL, $url.'?content='.$json);
+        }
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        if ($response === false) {
+            $this->error = 'Unable to connect to ' . $url;
+            return;
+        }
+
+        $this->output = $response;
+
+        return true;
+
     }
-
-    $opts = array(
-      'http' => array(
-        'method' => $method,
-        'header' => implode("\r\n", $headers),
-        'content' => $json,
-      )
-    );
-
-    $context = stream_context_create($opts);
-    #$response = @file_get_contents($url, false, $context);
-    $response = file_get_contents($url, false, $context);
-
-    if ($response === false)
-    {
-      $this->error = 'Unable to connect to ' . $url;
-      return;
-    }
-
-    $this->output = $response;
-
-    return true;
-
-  }
 
 }
 
